ff245f276f7a11df91d9178a0f9c1ce9ecfcb8a7
Lukasz <ubaczyk@gmail.com>
2018-11-13 13:59:21 +0100

    dodano katalog gier i gre

---
+++ zadanie1/zadanie1/Gra.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie1
+{
+    class Gra
+    {
+        private string tytul;
+        private string opis;
+
+        public Gra(string tytul, string opis)
+        {
+            this.tytul = tytul;
+            this.opis = opis;
+        }
+    }
+}
---
+++ zadanie1/zadanie1/WykazKlientow.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie1
+{
+    class WykazKlientow
+    {
+        private List<Klient> klienci = new List<Klient>();
+
+        public void dodajKlienta(string imie, string nazwisko, string numerDowodu)
+        {
+            klienci.Add( new Klient(imie, nazwisko, numerDowodu) );
+        }
+
+    }
+}
