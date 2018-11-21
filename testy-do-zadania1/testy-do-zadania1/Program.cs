f96cc798c29ec79708f5181ee288e04770f6be80
Lukasz <ubaczyk@gmail.com>
2018-11-21 19:57:29 +0100

    utworzono projekt: testy do zadania 1

---
+++ testy-do-zadania1/testy-do-zadania1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testy_do_zadania1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
