8c9c7d2f3a3541fd86f4d326bb285948f781a67f
Lukasz <ubaczyk@gmail.com>
2019-01-09 14:10:14 +0100

    dodano testowy projekt WPF

---
+++ zadanie4/task4/WpfProject/ViewModels/CustomerViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WpfProject.Commands;
+using WpfProject.Models;
+
+namespace WpfProject.ViewModels
+{
+    internal class CustomerViewModel
+    {
+        private Customer _Customer;
+
+        public CustomerViewModel()
+        {
+            _Customer = new Customer("Lukasz");
+            UpdateCommand = new CustomerUpdateCommand(this);
+        }
+
+        public bool CanUpdate
+        {
+            get
+            {
+                if(Customer == null)
+                {
+                    return false;
+                }
+                return !String.IsNullOrWhiteSpace(Customer.Name);
+            }
+        }
+
+        public Customer Customer
+        {
+            get
+            {
+                return _Customer;
+            }
+        }
+
+        public ICommand UpdateCommand
+        {
+            get;
+            private set;
+        }
+
+        public void SaveChanges()
+        {
+            Debug.Assert(false, String.Format("{0} was updated.", Customer.Name));
+        }
+    }
+}
